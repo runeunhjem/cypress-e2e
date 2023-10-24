de4fc64f4291efdf9b6fdcb14d41f062d20e6476
runeunhjem <runeunhjem@gmail.com>
2023-10-24 14:34:10 +0200

    Adds cypruss CLI test

--- cypress/e2e/3-testing/no.wikipedia.cy.js
+++ cypress/e2e/3-testing/no.wikipedia.cy.js
@@ -1,11 +1,11 @@
 // Checks inside <h1> tag
-describe("no.wikipedia.org", () => {
-  it("can search for Noroff", () => {
-    cy.visit("https://no.wikipedia.org");
-    cy.get("input#searchInput").type("Noroff{enter}", { delay: 500 });
-    cy.get("h1").contains("Noroff");
-  });
-});
+// describe("no.wikipedia.org", () => {
+//   it("can search for Noroff", () => {
+//     cy.visit("https://no.wikipedia.org");
+//     cy.get("input#searchInput").type("Noroff{enter}", { delay: 500 });
+//     cy.get("h1").contains("Noroff");
+//   });
+// });
 
 // Checks inside <span> tag
 describe("no.wikipedia.org", () => {

4777d383932182c1b1190bfd62d2c3cb26739b8b
runeunhjem <runeunhjem@gmail.com>
2023-10-25 07:35:30 +0200

    Adds eslint/jest

---
+++ .history/cypress.config_20231025073519.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  viewportWidth: 1920, // Set your preferred width
+  viewportHeight: 1200, // Set your preferred height
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
